8b2c28b2f7451c7c09ed589a3c8b56384729a760
Hiago W. Petris <hiago.petris@gmail.com>
2022-10-31 09:01:09 -0300

    Custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nosso_primeiro_projeto/data/task_inherited.dart';
 import 'package:nosso_primeiro_projeto/screens/initial_screen.dart';
+import 'package:nosso_primeiro_projeto/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
+        // theme: tema,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
